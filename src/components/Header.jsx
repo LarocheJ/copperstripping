a6685684a742b7a4ea22428817b0b1652f97d167
Jimmy Laroche <jimmy@truemarket.ca>
2025-06-19 20:12:27 -0600

    Implement scroll effect in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 
 export default function Header() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            setScrolled(offset > 50);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [])
+
     return(
-        <header className="header container">
-            <span className="logo"><span className="color-copper">Copper</span>Stripping</span>
-            <nav className="nav">
-                <a className="nav__item" href="">How it works</a>
-                <a className="nav__item" href="">Pricing</a>
-                <a className="nav__item" href="">FAQ</a>
-                <a className="nav__item" href="">Contact</a>
-                <Button title="" link="#" />
-            </nav>
+        <header className={`header` + (scrolled ? " header--scrolled" : "")}>
+            <div className="container">
+                <span className="logo"><span className="color-copper">Copper</span>Stripping</span>
+                <nav className="nav">
+                    <a className="nav__item" href="">How it works</a>
+                    <a className="nav__item" href="">Pricing</a>
+                    <a className="nav__item" href="">FAQ</a>
+                    <a className="nav__item" href="">Contact</a>
+                    <Button title="" link="#" />
+                </nav>
+            </div>
         </header>
     )
 }
